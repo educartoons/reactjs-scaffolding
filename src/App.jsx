582fe81f4d3352017309342a3b10fabbf89dafd7
Eduar <educartoons@gmail.com>
2023-09-06 17:32:19 -0500

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import './style.css';
+
 function App() {
-  return <h1>Hello !</h1>;
+  return (
+    <div>
+      <div className="p-0 m-0">
+        <h1>Hello !</h1>
+      </div>
+    </div>
+  );
 }
 
 const root = createRoot(document.getElementById('root'));
